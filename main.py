c0d069b4b8055f0a00f33403923c01e8f28c47c6
Vozze42 <servaas.clerckx@gmail.com>
2020-05-14 14:44:25 +0200

    Populated files

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-secret_number = 420
+import pygame 
+import physics
+
+class SpaceShip(physics.Physics_Object):
+        def __init__(self, mass, radius, pos, vel, accel, ang_pos, ang_vel, ang_accel):
+                physics.Physics_Object.__init__(self, mass, radius, pos, vel, accel, ang_pos, ang_vel, ang_accel)
+
+
+class Asteroid(physics.Physics_Object):
+        def __init__(self, mass, radius, pos, vel, accel, ang_pos, ang_vel, ang_accel, points):
+                physics.Physics_Object.__init__(self, mass, radius, pos, vel, accel, ang_pos, ang_vel, ang_accel)
+                self.energy =  #assuming average mass = 200 kg
+                self.points = points #points player gets when asteroid gets destoryed
+
+
+class Ray():
+        def __init__(self, direction, pos): #pos is equal to the center position of the spacecraft
+                self.pos  = pos #[x, y]
+                self.vel = math.cosvel #[vx, vy]
+                impulse = 10 #enough to slow avg astronaut down by ...
+                energy = 100 #damage done
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+is_blue = True
+x = 30
+y = 30
+clock = pygame.time.Clock()
+
+player = SpaceShip(10,1,[0,0],[0,0],[0,0],0,0,0)
+
+while not done:
+        for event in pygame.event.get():
+                pass
+
+        player.add_force([100,0])
+        player.physics_update(1/60)
+        print(player.pos)
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+        clock.tick(60)
+
---
+++ physics_engine.py
@@ -0,0 +1,60 @@
+import math
+
+class Vector2:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return "({0},{1})".format(self.x,self.y)
+    
+    def __add__(self,other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2(x,y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector2(x,y)
+
+    def __mul__(self, other):
+        x = self.x * other
+        y = self.y * other
+        return Vector2(x,y) 
+
+    def __div__(self, other):
+        x = self.x / other
+        y = self.y / other
+        return Vector2(x,y) 
+
+    def __iadd__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2(x,y) 
+
+class Physics_Object:
+    physics_objects = []
+
+    def __init__(self, mass = 1, pos = Vector2(0,0), vel = Vector2(0,0), accel = Vector2(0,0), moi = 1, ang = 0, ang_vel = 0, ang_accel = 0):
+        self.mass = mass
+        self.moi = moi
+        self.pos = pos
+        self.vel = vel
+        self.accel = accel
+        self.ang = ang
+        self.ang_vel = ang_vel
+        self.ang_accel = ang_accel
+
+        self.forces = []
+        self.physics_objects.append(self)
+
+    def add_force(self, force):
+        self.forces.append(force)
+
+    def physics_update(self, dt):
+        for force in self.forces:
+            self.accel += force / self.mass
+        self.vel += self.accel * dt
+        self.pos += self.vel * dt
+        self.accel = Vector2()
